8c655213dfb34a717f5bd1e72fd35f01f10ad4ed
Lucas <morettilucash@gmail.com>
2019-11-20 20:06:07 -0300

    server3

--- server/config/config.js
+++ server/config/config.js
@@ -14,7 +14,7 @@ let urlDB
 if(process.env.NODE_ENV == 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://lucasdb:lucasdb@cluster0-rpsop.mongodb.net/cafe'
+    urlDB = 'mongodb+srv://lucasdb:lucasdb@cluster0-rpsop.mongodb.net/cafe?retryWrites=true&w=majority'
 }
 
 process.env.URLDB = urlDB;
